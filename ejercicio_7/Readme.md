1f95a29762772a268bee0e313620e078ef665364
Miguelin69 <miguel2234505@correo.uis.edu.co>
2023-03-10 22:59:15 -0500

    se agrego programa

---
+++ ejercicio_7/Readme.md
@@ -0,0 +1,8 @@
+## EJERCICIO NÚMERO 7
+Programa para determinar si dados tres números enteros, la suma de los
+dos primeros es igual al tercero.
+
+# Analisis
+
+# Diseño
+![Diagrama de flujo](diagramaFE.png "Diagrama de flujo")
---
+++ ejercicio_7/ejercicio-7.py
@@ -0,0 +1,8 @@
+A = int(input("Ingrese el valor del primer numero: "))
+B = int(input("Ingrese el valor del segundo numero: "))
+C = int(input("Ingrese el valor del tercer numero: "))
+
+if C == (B+A):
+    print(f"El numero {C} es la suma de B + A")
+else :
+    print("El ultimo número no es la suma de B + A")
