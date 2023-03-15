8dd72719282b4eae36db87e6a7f0d5b30dbfc565
Miguelin69 <miguel2234505@correo.uis.edu.co>
2023-03-15 13:46:21 -0500

    se agrego programa

---
+++ ejercicio_8/Readme.md
@@ -0,0 +1,8 @@
+## EJERCICIO NÚMERO 8
+Porgrama para saber si un numero es multiplo del otro
+
+# Analisis
+
+# Diseño
+
+![Diagrama de flujo](diagramaSF.png "Diagrama de flujo")
---
+++ ejercicio_8/ejercicio-8.py
@@ -0,0 +1,12 @@
+print("Por favor ingresa el primer numero: ")
+a = int(input())
+print("Por favor ingresa el segundo numero: ")
+b = int(input())
+
+
+if a % b == 0:
+    print("Los numeros son MULTIPLOS ")
+
+
+else:
+    print("Los números no son MULTIPLOS ")
